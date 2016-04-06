e04b2c41f025a12b490ebb4bc7b1d310eda2fc5b
eryshev <a.a.eryshev@gmail.com>
2016-04-06 21:14:56 +0200

    Add simple config

--- auwch.py
+++ auwch.py
@@ -4,13 +4,25 @@ import requests
 import datetime
 import os
 import subprocess
+import json
+import sys
+import logging
 
-class ImageRetriever():
-    def desktopprImgRetrieve():
-        image_base_dir = 'images'
-        base_url = 'https://api.desktoppr.co'
-        # requests.get('{}/{}'.format(base_url, '1/wallpapers/random?safe_filter=all'))
-        img_data_url = '{}/{}'.format(base_url, '1/wallpapers/random')
+class ConfService:
+    def __init__(self, conf_uri): 
+        self.conf_dict = json.load(open(conf_uri, 'r'))
+        
+    def get_conf(self):
+        return self.conf_dict
+
+class ImageRetriever:
+    def desktopprImgRetrieve(conf_service):
+        conf = conf_service.get_conf() 
+        image_base_dir = conf['image_base_dir']
+        base_url = conf['desktoppr']['base_url']
+        safe = conf['desktoppr']['safe']
+        img_data_url = '{}/{}'.format(base_url, conf['desktoppr']['safe_path']) if safe else '{}/{}'.format(base_url, conf['desktoppr']['not_safe_path'])
+        logging.info('api url to call {}'.format(img_data_url))
         img_data_resp = requests.get(img_data_url)
         img_data = img_data_resp.json()
         img_url = img_data['response']['image']['url']
@@ -26,7 +38,7 @@ class ImageRetriever():
         img_file_abs_path = os.path.abspath('{}/{}'.format(os.path.curdir, img_file_rel_path))
         return img_file_abs_path
 
-class WallpaperChanger(): 
+class WallpaperChanger: 
     def changeWallpaper(img_file_abs_path):
         command = 'gsettings'
         sub_command = 'set'
@@ -36,7 +48,17 @@ class WallpaperChanger():
         subprocess.run([command, sub_command, schema, arg, file_uri])
        
 def main(): 
-    img_file_abs_path = ImageRetriever.desktopprImgRetrieve()
+    conf_uri = ''
+    if (len(sys.argv) == 2):
+        conf_uri = sys.argv[1]
+    else:
+        conf_uri = 'application.conf'
+        
+    conf_service = ConfService(conf_uri)
+    conf = conf_service.get_conf()
+    logging.basicConfig(filename = conf['log_file_path'], level = logging.INFO)
+    
+    img_file_abs_path = ImageRetriever.desktopprImgRetrieve(conf_service)
     WallpaperChanger.changeWallpaper(img_file_abs_path)
 
 if __name__ == '__main__':
